bcc36432016d11dfead1236f23b0ce5c2decc377
konbu33 <konbu33@example.com>
2022-07-30 09:39:50 +0900

    test: UserIdクラスのテスト実装

---
+++ test/unit_test/userid_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stackremote/userid.dart';
+import 'package:ulid/ulid.dart';
+
+void main() {
+  group("UserIdインスタンス生成テスト", () {
+    test("useridのファクトリメソッドでインスタンス生成すると、ulidが採番されていること", () {
+      // when
+      final userId = UserId.create();
+
+      // then
+      expect(userId.value, isA<Ulid>());
+      // print("userId : ${userId.value.runtimeType}");
+      // print("userId : ${userId.value}");
+    });
+
+    test("useridの生成的コンストラクタ,プライベートコンストラクタを利用したインスタンス生成が不可なこと", () {
+      // when
+      // final userId = UserId(value: Ulid());
+      // final userId = UserId._(value: Ulid());
+
+      // then
+      // print("userId : ${userId.value}");
+    });
+  });
+}
