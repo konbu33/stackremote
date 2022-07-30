2adcf117d031a6f2fc9ca6e4998b6b564c58b7d9
konbu33 <konbu33@example.com>
2022-07-30 09:50:26 +0900

    test: Usersクラスのテスト実装

---
+++ test/unit_test/users_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stackremote/user.dart';
+import 'package:stackremote/users.dart';
+
+void main() {
+  group("usersインスタンス生成テスト", () {
+    test('createメソッドでインスタンス生成した場合、users属性の値が空リストであること', () {
+      // given
+      // when
+      final users = Users.create();
+
+      // then
+      expect(users.users, []);
+    });
+    test("usersの生成的コンストラクタ,プライベートコンストラクタを利用したインスタンス生成が不可なこと", () {
+      // when
+      // final users = Users();
+      // final users = Users._(users: []);
+
+      // then
+      // print("users : ${users.value}");
+    });
+
+    test('reconstructメソッドでインスタンス生成した場合、users属性の値が引数の値と同じであること', () {
+      // given
+      final List<User> userList = [
+        User.create(email: "ake@test.com", password: "ake"),
+        User.create(email: "ike@test.com", password: "ike"),
+        User.create(email: "uke@test.com", password: "uke"),
+      ];
+
+      // when
+      final users = Users.reconstruct(users: userList);
+
+      // then
+      expect(users.users, userList);
+      expect(users.users[0] == userList[0], isTrue);
+    });
+  });
+}
