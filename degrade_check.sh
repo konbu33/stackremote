c2184463d0c69225f308c43243c64808caa64c35
konbu33 <konbu33@example.com>
2022-08-05 10:10:02 +0900

    refactor: flutter pub upgrade実施

---
+++ degrade_check.sh
@@ -0,0 +1,81 @@
+echo "
+# --------------------------------------------------
+#
+# flutter clean and pub get
+#
+# --------------------------------------------------
+"
+flutter clean
+flutter pub get
+
+
+echo "
+# --------------------------------------------------
+#
+# build_runner
+#
+# --------------------------------------------------
+"
+flutter pub run build_runner build --delete-conflicting-outputs
+
+
+# --------------------------------------------------
+#
+# flutter test 
+#
+# --------------------------------------------------
+# flutter test test/goldens_test test/widget_test test/unit_test
+
+echo "
+# --------------------------------------------------
+#
+# flutter unit test 
+#
+# --------------------------------------------------
+"
+flutter test test/unit_test
+
+
+
+echo "
+# --------------------------------------------------
+#
+# flutter widget test 
+#
+# --------------------------------------------------
+"
+flutter test test/widget_test
+
+
+
+echo "
+# --------------------------------------------------
+#
+# flutter goldens test 
+#
+# --------------------------------------------------
+"
+flutter test test/goldens_test 
+
+
+
+echo "
+# --------------------------------------------------
+#
+# flutter integration_test 
+#
+# --------------------------------------------------
+"
+# flutter drive --driver=test_driver/integration_test.dart --target=integration_test/home_page_test.dart
+
+
+
+# --------------------------------------------------
+#
+# flutter pub deps
+#
+# --------------------------------------------------
+# flutter pub outdated --color
+# pkg="meta";
+# flutter pub deps -s list | grep -E "^- |$pkg" | grep -v "\- $pkg ^" | grep -E "\- $pkg" -B 1
+
