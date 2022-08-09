e696f70867f6ede07c798a17380a412f2c8c3367
konbu33 <konbu33@example.com>
2022-08-09 10:51:25 +0900

    refactor: providers.dartからrouter_widget.dartへrouter関連のコードを凝集

--- lib/authentication/application/providers.dart
+++ lib/authentication/application/providers.dart
@@ -1,15 +1,8 @@
-// import '../../todo/presentation/todo_list_page.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:stackremote/user_detail_page.dart';
 
-import '../../user_page.dart';
-import '../presentation/page/home_page.dart';
-import '../presentation/page/signin_page.dart';
 import '../presentation/page/signin_page_notifier.dart';
 import '../presentation/page/signin_page_state.dart';
-import '../presentation/page/signup_page.dart';
 import '../presentation/page/signup_page_notifier.dart';
 import '../presentation/page/signup_page_state.dart';
 import '../presentation/widget/login_form_notifier.dart';
@@ -101,59 +94,6 @@ class Providers {
       AuthenticationServiceFirebase, AuthenticationServiceState>((ref) {
     return AuthenticationServiceFirebase();
   });
-
-  static final routerProvider = Provider(
-    (ref) {
-      return GoRouter(
-        initialLocation: '/',
-        routes: [
-          GoRoute(path: '/', builder: (context, state) => const UserPage()),
-          GoRoute(path: '/home', builder: (context, state) => const HomePage()),
-          GoRoute(
-              path: '/signin', builder: (context, state) => const SignInPage()),
-          GoRoute(
-              path: '/signup', builder: (context, state) => const SignUpPage()),
-          GoRoute(path: '/user', builder: (context, state) => const UserPage()),
-          GoRoute(
-              path: '/userdetail',
-              builder: (context, state) => const UserDetailPage()),
-          // GoRoute(
-          //     path: '/todolist',
-          //     builder: (context, state) => const TodoListPage()),
-          // GoRoute(
-          //   path: '/todo/detail/:id',
-          //   builder: (context, state) {
-          //     print("detail : ${state.params["id"]}");
-          //     return TodoDetailPage(todoIdString: state.params["id"]);
-          //   },
-          // ),
-        ],
-        redirect: (state) {
-          final loggedIn = ref.watch(authenticationServiceProvider).loggedIn;
-          print(
-              "call go_router ridirect : ------------------------------- : loggedIn : ${loggedIn} , subloc : ${state.subloc}");
-
-          if (!loggedIn) {
-            if (state.subloc == '/signin') {
-              return null;
-            } else if (state.subloc == '/signup') {
-              return null;
-            } else {
-              return '/signin';
-            }
-          } else {
-            if (state.subloc == '/signin') {
-              return '/';
-            } else if (state.subloc == '/signup') {
-              return '/';
-            } else {
-              return null;
-            }
-          }
-        },
-      );
-    },
-  );
 
   static final useAuthProvider = Provider.autoDispose((ref) {
     print(" ---------------- call useAuthProvider ------------------------- ");
--- lib/authentication/presentation/widget/router_widget.dart
+++ lib/authentication/presentation/widget/router_widget.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../user_detail_page.dart';
+import '../../../user_page.dart';
 import '../../application/providers.dart';
+import '../page/home_page.dart';
+import '../page/signin_page.dart';
+import '../page/signup_page.dart';
 
 class RouterWidget extends HookConsumerWidget {
   const RouterWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(Providers.routerProvider);
+    final router = ref.watch(routerProvider);
     // final themeData = ref.watch(themeProvider);
 
     return MaterialApp.router(
@@ -47,77 +52,60 @@ class RouterWidget extends HookConsumerWidget {
           iconTheme: IconThemeData(color: Colors.black26),
         ),
       ),
-
-// theme: ThemeData(
-//         primarySwatch: Colors.cyan,
-//         primaryTextTheme: const TextTheme(
-//           titleLarge: TextStyle(
-//             color: Colors.green,
-//           ),
-//         ),
-//         // backgroundColor: Colors.transparent,
-//         // scaffoldBackgroundColor: Colors.transparent,
-//         scaffoldBackgroundColor: Colors.white.withOpacity(0.8),
-//         inputDecorationTheme: InputDecorationTheme(
-//           // 入力フィールドの装飾
-//           // decoration: InputDecoration(
-//           // フィールド名
-//           // label: Text(state.loginFieldName),
-
-//           // 入力フィールドの色・枠
-//           // prefixIcon: state.loginIdIcon.value,
-//           border: OutlineInputBorder(
-//               borderRadius: BorderRadius.circular(30),
-//               borderSide: BorderSide.none),
-//           // fillColor: Colors.grey[100],
-//           // fillColor: Colors.transparent,
-//           fillColor: Colors.grey.withOpacity(0.3),
-//           filled: true,
-
-//           // ヘルパー・エラーメッセージ表示
-//           // counterText: "",
-//           // helperText: state.validateSuccess ? "Check : OK" : "",
-//           helperStyle: const TextStyle(color: Colors.cyan),
-//         ),
-//         // ),
-
-//         appBarTheme: const AppBarTheme(
-//           elevation: 0,
-//           // backgroundColor: Color.fromARGB(0, 0, 0, 0),
-//           backgroundColor: Colors.transparent,
-//           // foregroundColor: Colors.black87,
-//           // titleTextStyle: TextStyle(
-//           //   color: Colors.black87,
-//           // ),
-//           iconTheme: IconThemeData(color: Colors.black26),
-//           // actionsIconTheme: IconThemeData(color: Colors.green),
-//           // color: Colors.amber,
-//           // backgroundColor: Colors.amber.withOpacity(0.5),
-//           // foregroundColor: Colors.amber,
-//         ),
-//       ),
     );
   }
 }
 
-// final themeProvider = Provider((ref) {
-//   ThemeData themeData = ThemeData(
-//     primarySwatch: Colors.green,
-//     primaryTextTheme: const TextTheme(
-//       titleLarge: TextStyle(
-//         color: Colors.green,
-//       ),
-//     ),
-//     appBarTheme: const AppBarTheme(
-//       elevation: 0,
-//       titleTextStyle: TextStyle(
-//         color: Colors.black26,
-//       ),
-//       // color: Colors.amber,
-//       // backgroundColor: Colors.amber.withOpacity(0.5),
-//       // foregroundColor: Colors.amber,
-//     ),
-//   );
+final routerProvider = Provider(
+  (ref) {
+    return GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(path: '/', builder: (context, state) => const UserPage()),
+        GoRoute(path: '/home', builder: (context, state) => const HomePage()),
+        GoRoute(
+            path: '/signin', builder: (context, state) => const SignInPage()),
+        GoRoute(
+            path: '/signup', builder: (context, state) => const SignUpPage()),
+        GoRoute(path: '/user', builder: (context, state) => const UserPage()),
+        GoRoute(
+            path: '/userdetail',
+            builder: (context, state) => const UserDetailPage()),
+        // GoRoute(
+        //     path: '/todolist',
+        //     builder: (context, state) => const TodoListPage()),
+        // GoRoute(
+        //   path: '/todo/detail/:id',
+        //   builder: (context, state) {
+        //     print("detail : ${state.params["id"]}");
+        //     return TodoDetailPage(todoIdString: state.params["id"]);
+        //   },
+        // ),
+      ],
+      redirect: (state) {
+        final loggedIn =
+            ref.watch(Providers.authenticationServiceProvider).loggedIn;
+        print(
+            "call go_router ridirect : ------------------------------- : loggedIn : ${loggedIn} , subloc : ${state.subloc}");
 
-//   return themeData;
-// });
+        if (!loggedIn) {
+          if (state.subloc == '/signin') {
+            return null;
+          } else if (state.subloc == '/signup') {
+            return null;
+          } else {
+            return '/signin';
+          }
+        } else {
+          if (state.subloc == '/signin') {
+            return '/';
+          } else if (state.subloc == '/signup') {
+            return '/';
+          } else {
+            return null;
+          }
+        }
+      },
+    );
+  },
+);
