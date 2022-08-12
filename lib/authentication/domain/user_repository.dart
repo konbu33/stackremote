3ac43491076bdf09a5954b456b7e7b20a50257bc
konbu33 <konbu33@example.com>
2022-08-12 09:46:05 +0900

    refactor: ディレクトリ移動

---
+++ lib/authentication/domain/user_repository.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'user.dart';
+import 'userid.dart';
+// import 'users.dart';
+
+typedef JsonMap = Map<String, dynamic>;
+
+abstract class UserRepository {
+  UserRepository({
+    required this.firebaseFirestoreInstance,
+  });
+
+  final FirebaseFirestore firebaseFirestoreInstance;
+
+  late CollectionReference<JsonMap> ref;
+
+  Future<UserId> add(User user);
+  // Future<String> delete(String userId);
+  // void update(User user);
+  // Stream<Users> fetchAll();
+  // Future<User> fetchById(String userId);
+}
