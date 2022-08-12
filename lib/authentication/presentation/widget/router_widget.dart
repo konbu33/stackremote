cf15bd20d56c6d6fd14d244b7c54e413f4d21a3b
konbu33 <konbu33@example.com>
2022-08-12 09:26:08 +0900

    refactor: 不要なコメント行を削除

--- lib/authentication/presentation/widget/router_widget.dart
+++ lib/authentication/presentation/widget/router_widget.dart
@@ -15,25 +15,17 @@ class RouterWidget extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
-    // final themeData = ref.watch(themeProvider);
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
-      // theme: themeData,
-      // theme: ref.watch(themeProvider),
-      // darkTheme: ThemeData.dark(),
-      // darkTheme: themeData,
       theme: ThemeData.from(
         colorScheme: const ColorScheme.light(primary: Colors.cyan),
-        // textTheme: Typography.material2018().black,
-        // textTheme: GoogleFonts.notoSansJavaneseTextTheme(
         textTheme: GoogleFonts.mPlus1TextTheme(
           Typography.material2018().black,
         ),
-        // notoSansJavanese,
       ).copyWith(
         scaffoldBackgroundColor: Colors.white.withOpacity(0.8),
         inputDecorationTheme: InputDecorationTheme(
@@ -61,7 +53,6 @@ final routerProvider = Provider(
       initialLocation: '/',
       routes: [
         GoRoute(path: '/', builder: (context, state) => const UserPage()),
-        // GoRoute(path: '/home', builder: (context, state) => const HomePage()),
         GoRoute(
             path: '/signin', builder: (context, state) => const SignInPage()),
         GoRoute(
@@ -70,16 +61,6 @@ final routerProvider = Provider(
         GoRoute(
             path: '/userdetail',
             builder: (context, state) => const UserDetailPage()),
-        // GoRoute(
-        //     path: '/todolist',
-        //     builder: (context, state) => const TodoListPage()),
-        // GoRoute(
-        //   path: '/todo/detail/:id',
-        //   builder: (context, state) {
-        //     print("detail : ${state.params["id"]}");
-        //     return TodoDetailPage(todoIdString: state.params["id"]);
-        //   },
-        // ),
       ],
       redirect: (state) {
         final loggedIn =
