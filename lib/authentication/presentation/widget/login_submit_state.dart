a37bfa4655cf601102c584aadd9a63ff898476aa
konbu33 <konbu33@gmail.com>
2022-08-24 10:07:48 +0900

    refactor: Widegetを保持しないため、削除

--- lib/authentication/presentation/widget/login_submit_state.dart
+++ lib/authentication/presentation/widget/login_submit_state.dart
@@ -1,5 +1,3 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -14,7 +12,6 @@ part 'login_submit_state.freezed.dart';
 @freezed
 class LoginSubmitState with _$LoginSubmitState {
   const factory LoginSubmitState._({
-    required Widget loginSubmitWidget,
     required String loginSubmitWidgetName,
     required Function onSubmit,
   }) = _LoginSubmitState;
@@ -25,7 +22,6 @@ class LoginSubmitState with _$LoginSubmitState {
   }) =>
       LoginSubmitState._(
         loginSubmitWidgetName: loginSubmitWidgetName,
-        loginSubmitWidget: const Placeholder(),
         onSubmit: onSubmit,
       );
 }
