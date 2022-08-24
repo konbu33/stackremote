551e84bc4da22ea1120e0695a119adf2b4f3b809
konbu33 <konbu33@gmail.com>
2022-08-24 10:26:28 +0900

    refactor: SignInPageのWidget生成の責務をstateからpageへ移行

--- lib/authentication/presentation/page/signin_page_state.dart
+++ lib/authentication/presentation/page/signin_page_state.dart
@@ -1,17 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../authentication_service_firebase.dart';
 import '../../usecase/authentication_service_signin_usecase.dart';
 import '../widget/login_submit_state.dart';
-import '../widget/login_submit_widget.dart';
 import '../widget/loginid_field_state.dart';
-import '../widget/loginid_field_widget.dart';
 import '../widget/password_field_state.dart';
-import '../widget/password_field_widget.dart';
-import '../widget/signup_widget.dart';
 
 part 'signin_page_state.freezed.dart';
 
@@ -23,45 +18,33 @@ part 'signin_page_state.freezed.dart';
 @freezed
 class SignInPageState with _$SignInPageState {
   const factory SignInPageState._({
-    // SignUp Widget
-    required Widget singUpWidget,
-
     // Login Id Field Widget
-    required Widget loginIdField,
     required StateNotifierProvider<LoginIdFieldStateNotifier, LoginIdFieldState>
         loginIdFieldStateProvider,
 
     // Password Field Widget
-    required Widget passwordField,
     required StateNotifierProvider<PasswordFieldStateNotifier,
             PasswordFieldState>
         passwordFieldStateProvider,
 
     // Login Submit Widget
     @Default("サインイン") String loginSubmitWidgetName,
-    required Widget loginSubmitWidget,
     required StateNotifierProvider<LoginSubmitStateNotifier, LoginSubmitState>
         loginSubmitStateProvider,
   }) = _SignInPageState;
 
   factory SignInPageState.create() => SignInPageState._(
-        // SignUp Widget
-        singUpWidget: const Placeholder(),
-
         // Login Id Field Widget
-        loginIdField: const Placeholder(),
         loginIdFieldStateProvider:
             StateNotifierProvider<LoginIdFieldStateNotifier, LoginIdFieldState>(
                 (ref) => LoginIdFieldStateNotifier()),
 
         // Password Field Widget
-        passwordField: const Placeholder(),
         passwordFieldStateProvider: StateNotifierProvider<
             PasswordFieldStateNotifier,
             PasswordFieldState>((ref) => PasswordFieldStateNotifier()),
 
         // Login Submit
-        loginSubmitWidget: const Placeholder(),
         loginSubmitStateProvider:
             StateNotifierProvider<LoginSubmitStateNotifier, LoginSubmitState>(
           (ref) => LoginSubmitStateNotifier(
@@ -83,7 +66,6 @@ class SignInPageState with _$SignInPageState {
 // --------------------------------------------------
 class SignInPageStateNotifier extends StateNotifier<SignInPageState> {
   SignInPageStateNotifier() : super(SignInPageState.create()) {
-    rebuild();
   }
 
   // initial
@@ -91,48 +73,8 @@ class SignInPageStateNotifier extends StateNotifier<SignInPageState> {
     state = SignInPageState.create();
   }
 
-  // Rebuild
-  void rebuild() {
-    buildSignUpWidget();
-    buildLoginIdField();
-    buildPasswordField();
-    buildLoginSubmitWidget();
-  }
 
-  // SignUp Widget
-  void buildSignUpWidget() {
-    const Widget widget = SignUpWidget();
 
-    state = state.copyWith(singUpWidget: widget);
-  }
-
-  // Login Id Field Widget
-  void buildLoginIdField() {
-    Widget widget = LoginIdFieldWidget(
-      loginIdFieldstateProvider: state.loginIdFieldStateProvider,
-    );
-
-    state = state.copyWith(loginIdField: widget);
-  }
-
-  // Password Field Widget
-  void buildPasswordField() {
-    final Widget widget = PasswordFieldWidget(
-      passwordFieldStateProvider: state.passwordFieldStateProvider,
-    );
-
-    state = state.copyWith(passwordField: widget);
-  }
-
-  // Login Submit Widget
-  void buildLoginSubmitWidget() {
-    final Widget widget = LoginSubmitWidget(
-      loginIdFieldStateProvider: state.loginIdFieldStateProvider,
-      passwordFieldStateProvider: state.passwordFieldStateProvider,
-      loginSubmitStateProvider: state.loginSubmitStateProvider,
-    );
-
-    state = state.copyWith(loginSubmitWidget: widget);
   }
 }
 
