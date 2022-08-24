11a6922e08771799435757ba313cab03073fcfd6
konbu33 <konbu33@gmail.com>
2022-08-24 10:30:33 +0900

    refactor: SignUpPageのWidget生成の責務をstateからpageへ移行

--- lib/authentication/presentation/page/signup_page.dart
+++ lib/authentication/presentation/page/signup_page.dart
@@ -4,6 +4,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../widget/background_image_widget.dart';
 import '../widget/base_layout_widget.dart';
 
+import '../widget/login_submit_widget.dart';
+import '../widget/loginid_field_widget.dart';
+import '../widget/password_field_widget.dart';
 import 'signup_page_state.dart';
 
 class SignUpPage extends HookConsumerWidget {
@@ -13,10 +16,13 @@ class SignUpPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(signUpPageStateNotifierProvider);
 
+    final loginSubmitWidgetName =
+        ref.read(state.loginSubmitStateProvider).loginSubmitWidgetName;
+
     return BackgroundImageWidget(
       child: Scaffold(
         appBar: AppBar(
-          title: Text(state.loginSubmitWidgetName),
+          title: Text(loginSubmitWidgetName),
         ),
         body: BaseLayoutWidget(
           children: [
@@ -24,11 +30,11 @@ class SignUpPage extends HookConsumerWidget {
               key: GlobalKey<FormState>(),
               child: Column(
                 children: [
-                  state.loginIdField,
+                  SignUpPageWidgets.loginIdField(state),
                   const SizedBox(height: 30),
-                  state.passwordField,
+                  SignUpPageWidgets.passwordField(state),
                   const SizedBox(height: 40),
-                  state.loginSubmitWidget,
+                  SignUpPageWidgets.loginSubmitWidget(state),
                 ],
               ),
             ),
@@ -36,5 +42,33 @@ class SignUpPage extends HookConsumerWidget {
         ),
       ),
     );
+  }
+}
+
+class SignUpPageWidgets {
+  // Login Id Field Widget
+  static Widget loginIdField(SignUpPageState state) {
+    final Widget widget = LoginIdFieldWidget(
+      loginIdFieldstateProvider: state.loginIdFieldStateProvider,
+    );
+    return widget;
+  }
+
+  // Password Field Widget
+  static Widget passwordField(SignUpPageState state) {
+    final Widget widget = PasswordFieldWidget(
+      passwordFieldStateProvider: state.passwordFieldStateProvider,
+    );
+    return widget;
+  }
+
+  // Login Submit Widget
+  static Widget loginSubmitWidget(SignUpPageState state) {
+    final Widget widget = LoginSubmitWidget(
+      loginIdFieldStateProvider: state.loginIdFieldStateProvider,
+      passwordFieldStateProvider: state.passwordFieldStateProvider,
+      loginSubmitStateProvider: state.loginSubmitStateProvider,
+    );
+    return widget;
   }
 }
