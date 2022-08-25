0b1e977da2beda5a52d6cac34de20c80239fc33f
konbu33 <konbu33@gmail.com>
2022-08-25 09:47:24 +0900

    refactor: UserPageのユーザリストウィジェットを部品化し、別ファイルへ移動

---
+++ lib/user_list_widget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'user.dart';
+import 'user_detail_page.dart';
+import 'user_detail_page_state.dart';
+import 'user_page_state.dart';
+
+class UserListWidget extends StatelessWidget {
+  const UserListWidget({
+    Key? key,
+    required this.state,
+  }) : super(key: key);
+
+  final UserPageState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        // 全ユーザ情報取得
+        final usersStream = ref.watch(state.usersStreamProvider);
+        return usersStream.when(
+          // ローディング時
+          loading: () => const CircularProgressIndicator(),
+
+          // エラー時
+          error: (error, stacktrace) => const Text("error"),
+
+          // データ取得成功時
+          data: (data) {
+            return Flexible(
+              // 全ユーザの一覧生成
+              child: ListView.builder(
+                itemCount: data.users.length,
+                itemBuilder: (context, index) {
+                  final user = data.users[index];
+                  final email = user.email;
+                  final userId = user.userId.value.toString();
+
+                  // 各ユーザ毎の情報生成
+                  return ListTile(
+                    title: Text(email),
+                    subtitle: Text(userId),
+
+                    // タップされたユーザ情報を削除
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        state.userDeleteUseCase.execute(userId);
+                      },
+                    ),
+
+                    // タップされたユーザ情情を更新
+                    onTap: () async {
+                      // タップされたユーザ情情を取得
+                      final User user =
+                          await state.userFindByIdUseCase.execute(userId);
+
+                      // UserDetailPageStateのメソッド利利したいため
+                      final notifier = ref
+                          .read(userDetailPageStateControllerProvider.notifier);
+
+                      // タップされたユーザ情情を初期値として設定
+                      notifier.setUserEmailAndPassword(user);
+
+                      //ModalBottomSheet処理内でのonSubmit処理を最終確定
+                      notifier.setUserUpdateOnSubmit(user);
+
+                      // ModalBottomSheetでの処理
+                      await showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return const UserDetailPage();
+                        },
+                      );
+
+                      // 初期化処理
+                      notifier.clearUserEmailAndPassword();
+                    },
+                  );
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
--- lib/user_page.dart
+++ lib/user_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:stackremote/user_add_icon_widget.dart';
+import 'package:stackremote/user_list_widget.dart';
 
 import 'authentication/presentation/widget/signout_widget.dart';
-import 'user.dart';
-import 'user_detail_page.dart';
 import 'user_detail_page_state.dart';
 import 'user_page_state.dart';
 
@@ -35,7 +34,9 @@ class UserPage extends HookConsumerWidget {
 
 class UserPageWidgets {
   // pageTitleWidget
-  static Widget pageTitleWidget(UserPageState state) {
+  static Widget pageTitleWidget(
+    UserPageState state,
+  ) {
     final Widget widget = Text(state.pageTitle);
 
     return widget;
@@ -64,51 +65,8 @@ class UserPageWidgets {
   static Widget userListWidget(
     UserPageState state,
   ) {
-    final widget = Consumer(
-      builder: (context, ref, child) {
-        final usersStream = ref.watch(state.usersStreamProvider);
-        return usersStream.when(
-          loading: () => const CircularProgressIndicator(),
-          error: (error, stacktrace) => const Text("error"),
-          data: (data) {
-            return Flexible(
-              child: ListView.builder(
-                  itemCount: data.users.length,
-                  itemBuilder: (context, index) {
-                    final user = data.users[index];
-                    final email = user.email;
-                    final userId = user.userId.value.toString();
-
-                    return ListTile(
-                      title: Text(email),
-                      subtitle: Text(userId),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          state.userDeleteUseCase.execute(userId);
-                        },
-                      ),
-                      onTap: () async {
-                        final User user =
-                            await state.userFindByIdUseCase.execute(userId);
-                        final notifier = ref.read(
-                            userDetailPageStateControllerProvider.notifier);
-                        notifier.setUserEmailAndPassword(user);
-                        notifier.setUserUpdateOnSubmit(user);
-                        await showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return const UserDetailPage();
-                          },
-                        );
-                        notifier.clearUserEmailAndPassword();
-                      },
-                    );
-                  }),
-            );
-          },
-        );
-      },
+    final widget = UserListWidget(
+      state: state,
     );
 
     return widget;
