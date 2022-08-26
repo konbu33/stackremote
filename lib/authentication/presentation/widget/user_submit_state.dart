ed265346ff2b1e1ac4b5f6de26899f0150b9a02b
konbu33 <konbu33@gmail.com>
2022-08-26 16:30:15 +0900

    refactor: 不使用なコード削除

--- lib/authentication/presentation/widget/user_submit_state.dart
+++ lib/authentication/presentation/widget/user_submit_state.dart
@@ -1,4 +1,3 @@
-// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
