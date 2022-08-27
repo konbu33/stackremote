263c1df29312909c2546591e8be101b1b2c29c65
konbu33 <konbu33@gmail.com>
2022-08-27 09:12:39 +0900

    refactor: CustomMouseCursorOverlayerStateNotifierProviderのコード移動

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,7 +1,4 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'custom_mouse_cursor/custom_mouse_cursor_overlayer_state.dart';
-
-import 'custom_mouse_cursor/custom_mouse_cursor_overlayer_state_notifier.dart';
 
 class Providers {
   const Providers();
@@ -10,9 +7,4 @@ class Providers {
       StateProvider<String>((ref) => "Hello World Riverpod.");
 
   static final testProvider = Provider((ref) => ref.read(helloWorldProvider));
-
-  static final CustomMouseCursorOverlayerStateNotifierProvider =
-      StateNotifierProvider<CustomMouseCursorOverlayerStateNotifier,
-              CustomMouseCursorOerlayerState>(
-          (ref) => CustomMouseCursorOverlayerStateNotifier());
 }
