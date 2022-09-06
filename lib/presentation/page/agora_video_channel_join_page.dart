ef62e150cb8dfa2673e51dac9d582c66abe25a3e
konbu33 <konbu33@gmail.com>
2022-09-06 09:06:15 +0900

    feat: チャンネル参加用のページのウィジェットを追加

--- lib/presentation/page/agora_video_channel_join_page.dart
+++ lib/presentation/page/agora_video_channel_join_page.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:stackremote/domain/user.dart';
 
-import '../widget/agora_video_token_create_widget.dart';
 import '../widget/appbar_action_icon_widget.dart';
 import '../widget/background_image_widget.dart';
 import '../widget/base_layout_widget.dart';
 
-import '../widget/loginid_field_widget.dart';
+import '../widget/channel_join_submit_state.dart';
+import '../widget/channel_join_submit_widget.dart';
+import '../widget/channel_name_field_state.dart';
+import '../widget/channel_name_field_widget.dart';
 import 'agora_video_channel_join_page_state.dart';
-import 'agora_video_page_state.dart';
 
 class AgoraVideoChannelJoinPage extends HookConsumerWidget {
   const AgoraVideoChannelJoinPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final joinChannelState =
-        ref.watch(agoraVideoChannelJoinPageStateNotifierProvider);
-
-    final state = ref.watch(agoraVideoPageStateNotifierProvider);
-    final notifier = ref.watch(agoraVideoPageStateNotifierProvider.notifier);
+    final state = ref.watch(agoraVideoChannelJoinPageStateNotifierProvider);
 
     return BackgroundImageWidget(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Join Channel"),
+          title: Text(state.pageTitle),
           actions: [
-            AgoraVideoChannelJoinPageWidgets.signOutIconButton(
-                joinChannelState),
+            AgoraVideoChannelJoinPageWidgets.signOutIconButton(),
           ],
         ),
         body: BaseLayoutWidget(
@@ -36,11 +34,19 @@ class AgoraVideoChannelJoinPage extends HookConsumerWidget {
               key: GlobalKey<FormState>(),
               child: Column(
                 children: [
-                  AgoraVideoChannelJoinPageWidgets.loginIdField(
-                      joinChannelState),
+                  AgoraVideoChannelJoinPageWidgets.channelNameFieldWidget(),
                   const SizedBox(height: 40),
-                  AgoraVideoChannelJoinPageWidgets.loginSubmitWidget(
-                      state, notifier),
+                  AgoraVideoChannelJoinPageWidgets.channelJoinSubmitWidget(),
+                  () {
+                    final user = ref.watch(userStateNotifierProvider);
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Text(
+                        "idToken: ${user}",
+                        maxLines: 10,
+                      ),
+                    );
+                  }(),
                 ],
               ),
             ),
@@ -53,31 +59,42 @@ class AgoraVideoChannelJoinPage extends HookConsumerWidget {
 
 class AgoraVideoChannelJoinPageWidgets {
   // signOutIconButton
-  static Widget signOutIconButton(AgoraVideoChannelJoinPageState state) {
-    final Widget widget = AppbarAcitonIconWidget(
-      appbarActionIconStateProvider: state.signOutIconStateProvider,
+  static Widget signOutIconButton() {
+    final Widget widget = Consumer(
+      builder: ((context, ref, child) {
+        final state = ref.watch(agoraVideoChannelJoinPageStateNotifierProvider);
+
+        return AppbarAcitonIconWidget(
+          appbarActionIconStateProvider: state.signOutIconStateProvider,
+        );
+      }),
     );
 
     return widget;
   }
 
   // Login Id Field Widget
-  static Widget loginIdField(AgoraVideoChannelJoinPageState state) {
-    final Widget widget = LoginIdFieldWidget(
-      loginIdFieldStateProvider: state.loginIdFieldStateProvider,
+  static Widget channelNameFieldWidget() {
+    final channelNameFieldStateProvider =
+        ChannelNameFieldStateNotifierProviderList
+            .channelNameFieldStateNotifierProvider;
+
+    final Widget widget = ChannelNameFieldWidget(
+      channelNameFieldStateProvider: channelNameFieldStateProvider,
     );
+
     return widget;
   }
 
   // Login Submit Widget
-  static Widget loginSubmitWidget(
-    AgoraVideoPageState state,
-    AgoraVideoPageStateNotifier notifier,
-  ) {
-    final Widget widget = AgoraVideoTokenCreateWidget(
-      state: state,
-      notifier: notifier,
+  static Widget channelJoinSubmitWidget() {
+    final channelJoinSubmitStateProvider = ChannelJoinSubmitStateProviderList
+        .channelJoinSubmitStateNotifierProvider;
+
+    final Widget widget = ChannelJoinSubmitWidget(
+      channelJoinSubmitStateProvider: channelJoinSubmitStateProvider,
     );
+
     return widget;
   }
 }
--- lib/presentation/page/agora_video_channel_join_page_state.dart
+++ lib/presentation/page/agora_video_channel_join_page_state.dart
@@ -1,15 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../usecase/authentication_service_signout_usecase.dart';
 import '../authentication_service_firebase.dart';
-import '../../usecase/authentication_service_signup_usecase.dart';
+// import '../../usecase/authentication_service_signup_usecase.dart';
 import '../widget/appbar_action_icon_state.dart';
-import '../widget/login_submit_state.dart';
-import '../widget/loginid_field_state.dart';
+// import '../widget/login_submit_state.dart';
+// import '../widget/loginid_field_state.dart';
 
 part 'agora_video_channel_join_page_state.freezed.dart';
 
@@ -21,30 +20,36 @@ part 'agora_video_channel_join_page_state.freezed.dart';
 @freezed
 class AgoraVideoChannelJoinPageState with _$AgoraVideoChannelJoinPageState {
   const factory AgoraVideoChannelJoinPageState._({
+    // PageTitle
+    required String pageTitle,
+
     // SignOutIcon Button
     required String signOutIconButtonName,
     required AuthenticationServiceSignOutUsecase
         authenticationServiceSignOutUsecase,
     required AppbarActionIconStateProvider signOutIconStateProvider,
 
-    // Login Id Field Widget
-    required LoginIdFieldStateProvider loginIdFieldStateProvider,
+    // // Login Id Field Widget
+    // required LoginIdFieldStateProvider loginIdFieldStateProvider,
 
     // Login Submit Widget
-    @Default("新規登録") String loginSubmitWidgetName,
-    required AuthenticationServiceSignUpUsecase
-        authenticationServiceSignUpUsecase,
-    required LoginSubmitStateProvider loginSubmitStateProvider,
+    // @Default("新規登録") String loginSubmitWidgetName,
+    // required AuthenticationServiceSignUpUsecase
+    //     authenticationServiceSignUpUsecase,
+    // required LoginSubmitStateProvider loginSubmitStateProvider,
   }) = _AgoraVideoChannelJoinPageState;
 
   factory AgoraVideoChannelJoinPageState.create() =>
       AgoraVideoChannelJoinPageState._(
+        // PageTitle
+        pageTitle: "チャンネル参加",
+
         // Sign Out Button
         signOutIconButtonName: "サインアウト",
         authenticationServiceSignOutUsecase:
             AuthenticationServiceSignOutUsecase(
           authenticationService: AuthenticationServiceFirebase(
-            instance: FirebaseAuth.instance,
+            instance: firebase_auth.FirebaseAuth.instance,
           ),
         ),
         signOutIconStateProvider: appbarActionIconStateProviderCreator(
@@ -53,18 +58,18 @@ class AgoraVideoChannelJoinPageState with _$AgoraVideoChannelJoinPageState {
           onSubmit: () {},
         ),
 
-        // Login Id Field Widget
-        loginIdFieldStateProvider: loginIdFieldStateNotifierProviderCreator(),
+        // // Login Id Field Widget
+        // loginIdFieldStateProvider: loginIdFieldStateNotifierProviderCreator(),
 
-        // Login Submit
-        authenticationServiceSignUpUsecase: AuthenticationServiceSignUpUsecase(
-            authenticationService: AuthenticationServiceFirebase(
-                instance: firebase_auth.FirebaseAuth.instance)),
+        // // Login Submit
+        // authenticationServiceSignUpUsecase: AuthenticationServiceSignUpUsecase(
+        //     authenticationService: AuthenticationServiceFirebase(
+        //         instance: firebase_auth.FirebaseAuth.instance)),
 
-        loginSubmitStateProvider: loginSubmitStateNotifierProviderCreator(
-          loginSubmitWidgetName: "",
-          onSubmit: () {},
-        ),
+        // loginSubmitStateProvider: loginSubmitStateNotifierProviderCreator(
+        //   loginSubmitWidgetName: "",
+        //   onSubmit: () {},
+        // ),
       );
 }
 
@@ -87,30 +92,30 @@ class AgoraVideoChannelJoinPageStateNotifier
   // initial
   void initial() {
     state = AgoraVideoChannelJoinPageState.create();
-    setOnSubmit();
+    // setOnSubmit();
     setSignOutIconOnSubumit();
   }
 
-  void setOnSubmit() {
-    Function buildOnSubmit() {
-      return ({
-        required BuildContext context,
-      }) {
-        final email = ref
-            .read(state.loginIdFieldStateProvider)
-            .loginIdFieldController
-            .text;
-        // state.authenticationServiceSignUpUsecase.execute(email);
+  // void setOnSubmit() {
+  //   Function buildOnSubmit() {
+  //     return ({
+  //       required BuildContext context,
+  //     }) {
+  //       final email = ref
+  //           .read(state.loginIdFieldStateProvider)
+  //           .loginIdFieldController
+  //           .text;
+  //       // state.authenticationServiceSignUpUsecase.execute(email);
 
-        initial();
-      };
-    }
+  //       initial();
+  //     };
+  //   }
 
-    state = state.copyWith(
-        loginSubmitStateProvider: loginSubmitStateNotifierProviderCreator(
-            loginSubmitWidgetName: state.loginSubmitWidgetName,
-            onSubmit: buildOnSubmit()));
-  }
+  //   state = state.copyWith(
+  //       loginSubmitStateProvider: loginSubmitStateNotifierProviderCreator(
+  //           loginSubmitWidgetName: state.loginSubmitWidgetName,
+  //           onSubmit: buildOnSubmit()));
+  // }
 
   // setSignOutIconOnSubumit
   void setSignOutIconOnSubumit() {
