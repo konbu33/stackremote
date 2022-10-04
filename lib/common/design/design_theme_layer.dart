67409f967b6834b55fcccad9569712fecb2fc11e
konbu33 <konbu33@gmail.com>
2022-10-05 06:14:54 +0900

    refactor: 未使用な古いコード削除

--- lib/common/design/design_theme_layer.dart
+++ lib/common/design/design_theme_layer.dart
@@ -14,7 +14,6 @@ class DesignThemeLayer extends SingleChildStatelessWidget {
   @override
   Widget buildWithChild(BuildContext context, Widget? child) {
     return Theme(
-      // Design Theme
       data: ThemeData.from(
         colorScheme: const ColorScheme.light(primary: Colors.cyan),
         textTheme: GoogleFonts.mPlus1TextTheme(
@@ -39,34 +38,5 @@ class DesignThemeLayer extends SingleChildStatelessWidget {
       ),
       child: child ?? const Text("no child"),
     );
-
-    // return MaterialApp(
-    //   debugShowCheckedModeBanner: false,
-    //   home: child,
-
-    //   // Design Theme
-    //   theme: ThemeData.from(
-    //     colorScheme: const ColorScheme.light(primary: Colors.cyan),
-    //     textTheme: GoogleFonts.mPlus1TextTheme(
-    //       Typography.material2018().black,
-    //     ),
-    //   ).copyWith(
-    //     scaffoldBackgroundColor: Colors.white.withOpacity(0.8),
-    //     inputDecorationTheme: InputDecorationTheme(
-    //       border: OutlineInputBorder(
-    //           borderRadius: BorderRadius.circular(30),
-    //           borderSide: BorderSide.none),
-    //       fillColor: Colors.grey.withOpacity(0.3),
-    //       filled: true,
-    //       helperStyle: const TextStyle(color: Colors.cyan),
-    //     ),
-    //     appBarTheme: const AppBarTheme(
-    //       elevation: 0,
-    //       backgroundColor: Colors.transparent,
-    //       foregroundColor: Colors.black87,
-    //       iconTheme: IconThemeData(color: Colors.black26),
-    //     ),
-    //   ),
-    // );
   }
 }
