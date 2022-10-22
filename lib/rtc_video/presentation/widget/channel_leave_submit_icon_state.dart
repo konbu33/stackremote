0254ba2fd1722b6ef04a3e86aa11ae04e8ea7582
konbu33 <konbu33@gmail.com>
2022-10-23 08:30:32 +0900

    refactor: importのパスを絶対パスから相対パスへ変更

--- lib/rtc_video/presentation/widget/channel_leave_submit_icon_state.dart
+++ lib/rtc_video/presentation/widget/channel_leave_submit_icon_state.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:stackremote/rtc_video/infrastructure/rtc_channel_leave_provider.dart';
-import 'package:stackremote/rtc_video/domain/rtc_channel_state.dart';
+
+import '../../domain/rtc_channel_state.dart';
+import '../../infrastructure/rtc_channel_leave_provider.dart';
 
 part 'channel_leave_submit_icon_state.freezed.dart';
 
