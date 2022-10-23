bf516dd8a4e8ba305408dd226822e6c8f2027761
konbu33 <konbu33@gmail.com>
2022-10-23 09:50:04 +0900

    refactor: 未使用なコード削除

--- lib/authentication/presentation/widget/login_submit_widget.dart
+++ lib/authentication/presentation/widget/login_submit_widget.dart
@@ -2,25 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'login_submit_state.dart';
-// import 'loginid_field_state.dart';
-// import 'password_field_state.dart';
 
 class LoginSubmitWidget extends HookConsumerWidget {
   const LoginSubmitWidget({
     Key? key,
-    // required this.loginIdFieldStateProvider,
-    // required this.passwordFieldStateProvider,
     required this.loginSubmitStateProvider,
   }) : super(key: key);
 
-  // final LoginIdFieldStateProvider loginIdFieldStateProvider;
-  // final PasswordFieldStateProvider passwordFieldStateProvider;
   final LoginSubmitStateProvider loginSubmitStateProvider;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final loginIdFieldState = ref.watch(loginIdFieldStateProvider);
-    // final passwordFieldState = ref.watch(passwordFieldStateProvider);
     final loginSubmitState = ref.watch(loginSubmitStateProvider);
 
     return Column(
@@ -36,14 +28,6 @@ class LoginSubmitWidget extends HookConsumerWidget {
             onPressed: loginSubmitState.onSubmit == null
                 ? null
                 : loginSubmitState.onSubmit!(context: context),
-            // onPressed: loginIdFieldState.loginIdIsValidate.isValid &&
-            //         passwordFieldState.passwordIsValidate.isValid
-            //     ? () {
-            //         loginSubmitState.onSubmit(
-            //           context: context,
-            //         );
-            //       }
-            //     : null,
             child: Text(loginSubmitState.loginSubmitWidgetName),
           ),
         ),
