75847a367d4da8c64c17064ff95eee2f1548408f
konbu33 <konbu33@gmail.com>
2022-10-29 10:33:55 +0900

    feat: user_add_usecaseは未使用のため、ひとまずコードをコメントアウト

--- lib/user/usecace/user_add_usecase.dart
+++ lib/user/usecace/user_add_usecase.dart
@@ -1,56 +1,18 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:stackremote/authentication/authentication.dart';
-import 'package:stackremote/rtc_video/rtc_video.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:stackremote/authentication/authentication.dart';
+// import 'package:stackremote/rtc_video/rtc_video.dart';
 
-import '../domain/user.dart';
+// import '../domain/user.dart';
 
-final userAddUsecaseProvider = Provider((ref) {
-  final rtcChannelState = ref.watch(
-      RtcChannelStateNotifierProviderList.rtcChannelStateNotifierProvider);
+// final userAddUsecaseProvider = Provider((ref) {
+//   final rtcChannelState = ref.watch(
+//       RtcChannelStateNotifierProviderList.rtcChannelStateNotifierProvider);
 
-  final firebaseAuthUser = ref.watch(firebaseAuthUserStateNotifierProvider);
+//   final firebaseAuthUser = ref.watch(firebaseAuthUserStateNotifierProvider);
 
-  Future<void> execute({
-    required String nickName,
-    required bool isHost,
-    Timestamp? joinedAt,
-    Timestamp? leavedAt,
-    required bool isOnLongPressing,
-    required Offset pointerPosition,
-  }) async {
-    final User user = User.create(
-      nickName: nickName,
-      isHost: isHost,
-      joinedAt: joinedAt,
-      leavedAt: leavedAt,
-      isOnLongPressing: isOnLongPressing,
-      pointerPosition: pointerPosition,
-    );
-
-    await FirebaseFirestore.instance
-        .collection('channels')
-        .doc(rtcChannelState.channelName)
-        .collection('users')
-        .doc(firebaseAuthUser.email)
-        .set(user.toJson());
-  }
-
-  return execute;
-});
-
-// class UserAddUseCase {
-//   // Constructor
-//   const UserAddUseCase({
-//     required this.userRepository,
-//   });
-
-//   // Repository
-//   final UserRepository userRepository;
-
-//   // UseCase Execute
-//   Future<User> execute({
+//   Future<void> execute({
 //     required String nickName,
 //     required bool isHost,
 //     Timestamp? joinedAt,
@@ -58,7 +20,6 @@ final userAddUsecaseProvider = Provider((ref) {
 //     required bool isOnLongPressing,
 //     required Offset pointerPosition,
 //   }) async {
-//     // Construct Dimain Model Object
 //     final User user = User.create(
 //       nickName: nickName,
 //       isHost: isHost,
@@ -68,11 +29,50 @@ final userAddUsecaseProvider = Provider((ref) {
 //       pointerPosition: pointerPosition,
 //     );
 
-//     const docId = "xxx@test.com";
-
-//     // Repository Execute
-//     await userRepository.set(docId: docId, user: user);
-
-//     return user;
+//     await FirebaseFirestore.instance
+//         .collection('channels')
+//         .doc(rtcChannelState.channelName)
+//         .collection('users')
+//         .doc(firebaseAuthUser.email)
+//         .set(user.toJson());
 //   }
-// }
+
+//   return execute;
+// });
+
+// // class UserAddUseCase {
+// //   // Constructor
+// //   const UserAddUseCase({
+// //     required this.userRepository,
+// //   });
+
+// //   // Repository
+// //   final UserRepository userRepository;
+
+// //   // UseCase Execute
+// //   Future<User> execute({
+// //     required String nickName,
+// //     required bool isHost,
+// //     Timestamp? joinedAt,
+// //     Timestamp? leavedAt,
+// //     required bool isOnLongPressing,
+// //     required Offset pointerPosition,
+// //   }) async {
+// //     // Construct Dimain Model Object
+// //     final User user = User.create(
+// //       nickName: nickName,
+// //       isHost: isHost,
+// //       joinedAt: joinedAt,
+// //       leavedAt: leavedAt,
+// //       isOnLongPressing: isOnLongPressing,
+// //       pointerPosition: pointerPosition,
+// //     );
+
+// //     const docId = "xxx@test.com";
+
+// //     // Repository Execute
+// //     await userRepository.set(docId: docId, user: user);
+
+// //     return user;
+// //   }
+// // }
