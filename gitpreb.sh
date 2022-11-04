5ad1a5bae0834c06bb241b7f239dc3a0e4e58e12
konbu33 <konbu33@gmail.com>
2022-11-04 11:39:10 +0900

    feat: 元のブランチへswitchするように修正

--- gitpreb.sh
+++ gitpreb.sh
@@ -6,8 +6,15 @@ git br -vv
 git pull --rebase origin dev
 git br -vv
 
+# get current branch name
+currentBranch=$(git br | tail -n1 | sed 's/* //g')
+echo "currentBranch : $currentBranch"
+
 # rebase from dev branch
 git sw dev
 git pull --rebase
 git br -vv
 
+# switch current branch
+git sw $currentBranch
+git br -vv
