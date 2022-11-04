40026cf3ad29b342c58674fda831ee75e7909ed6
konbu33 <konbu33@gmail.com>
2022-11-04 13:22:07 +0900

    refactor: 個別のproviderとして提供していた機能をrepositoryへ集約したため、個別のproviderを削除

--- lib/authentication/authentication.dart
+++ lib/authentication/authentication.dart
@@ -1,6 +1,4 @@
 // authentication_layer.dart
-export './infrastructure/auth_state_changes_provider.dart';
-export './infrastructure/firebase_auth_get_id_token_provider.dart';
 export './authentication_routing_layer.dart';
 export './domain/firebase_auth_user.dart';
 
