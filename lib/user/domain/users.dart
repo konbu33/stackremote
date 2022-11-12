d72d457a0b9db204b36cf982f6646aaf20073eb9
konbu33 <konbu33@gmail.com>
2022-11-12 10:51:52 +0900

    refactor: 未使用なコード削除。

--- lib/user/domain/users.dart
+++ lib/user/domain/users.dart
@@ -1,10 +1,6 @@
-// StateNotifier
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 // Freezed
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:stackremote/user/usecace/user_fetch_all_usecase.dart';
 import 'user.dart';
 
 part 'users.freezed.dart';
@@ -21,63 +17,37 @@ class Users with _$Users {
     required List<User> users,
   }) = _Users;
 
-  factory Users.create() => const Users._(users: []);
-
-  factory Users.reconstruct({
-    required List<User> users,
+  factory Users.create({
+    List<User>? users,
   }) =>
       Users._(
-        users: users,
+        users: users ?? [],
+      );
+
+  factory Users.reconstruct({
+    List<User>? users,
+  }) =>
+      Users._(
+        users: users ?? [],
       );
 }
 
-/*
 // --------------------------------------------------
 //
 // StateNotifier
 //
 // --------------------------------------------------
-class UsersController extends StateNotifier<Users> {
-  UsersController() : super(Users.create()) {
-    _getInitData();
-  }
-
-  void resetData(List<User> users) {
-    state = state.copyWith(users: users);
-  }
-
-  void _getInitData() {
-    state = state.copyWith(
-      users: [
-        User.create(email: "init@test.com", password: "password"),
-      ],
-    );
-  }
-
-  void reconstruct(List<User> users) {
-    state = state.copyWith(users: users);
-  }
+class UsersStateNotifier extends StateNotifier<Users> {
+  UsersStateNotifier() : super(Users.create());
 }
-*/
 
 // --------------------------------------------------
 //
-// StreamProvider
+// StateNotifierProvider
 //
 // --------------------------------------------------
 
-final usersStreamProvider = StreamProvider<Users>((ref) {
-// final usersStreamProvider = StreamProvider<QuerySnapshot>((ref) {
-  final usersStream = ref.watch(userFetchAllUsecaseProvider);
-
-  // final rtcChannelState = ref.watch(
-  //     RtcChannelStateNotifierProviderList.rtcChannelStateNotifierProvider);
-
-  // return FirebaseFirestore.instance
-  //     .collection('channels')
-  //     .doc(rtcChannelState.channelName)
-  //     .collection('users')
-  //     .snapshots();
-
-  return usersStream();
+final usersStateNotifierProvider =
+    StateNotifierProvider<UsersStateNotifier, Users>((ref) {
+  return UsersStateNotifier();
 });
