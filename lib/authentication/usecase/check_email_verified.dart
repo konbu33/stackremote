bca58148945b2714f25e7ed400994bc590f8f6d9
konbu33 <konbu33@gmail.com>
2022-11-19 09:40:34 +0900

    refactor: wait_email_verified_pageでuseEffectを利用してメールアドレスの検証処理をTimerで定期実行していた。 この処理のコードがあることで、コードの見通しが悪くなっていると感じたため、分離したいと考えた。 それに伴い、useEffectが利用できなくなった。 そして、useEffect内のエラー処理でTimerのキャンセルを実行していたが、このキャンセルが実現不可になった。 そのため、本check_email_verified.dart内にエラー処理を移し、Timerのキャンセル処理を実現することが必要になった。

--- lib/authentication/usecase/check_email_verified.dart
+++ lib/authentication/usecase/check_email_verified.dart
@@ -1,24 +1,22 @@
 import 'dart:async';
 
 // improve: この依存を解消したい。
-// import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../common/common.dart';
 import '../domain/firebase_auth_user.dart';
 import '../infrastructure/authentication_service.dart';
 import '../infrastructure/authentication_service_firebase.dart';
 
 final checkEmailVerifiedUsecaseProvider = Provider.autoDispose((ref) {
   //
-  bool isEmailVerified = false;
-
-  final AuthenticationService authenticationService =
-      ref.watch(authenticationServiceFirebaseProvider);
 
   Timer execute() {
     return Timer.periodic(
       const Duration(seconds: 3),
       (timer) async {
+        logger.d("checkEmailVerifiedTimer");
         // print(" ${DateTime.now()} : Timer During....................");
         // userChanges でFirebaseAuthのUserの状態変化を監視している場合
         // reload()実行するだけで、currentUserで取得できるemailVerified属性の最新の値が反映される。
@@ -27,16 +25,33 @@ final checkEmailVerifiedUsecaseProvider = Provider.autoDispose((ref) {
         // reload()実行するだけでは、currentUserで取得できるemailVerified属性の最新の値が反映さない。
         // reload() + currentUserの取得 により、currentUserで取得できるemailVerified属性の最新の値が反映される。
 
-        isEmailVerified =
-            await authenticationService.currentUserGetEmailVerified();
+        // pageでuseEffectを利用するとtry,catchして、timer.cancel()できたが、useEffect利用しない場合、
+        // pageでtry,catchして、timer.cancel()できなかったため、ここでcancelする必要がありそう。
+        try {
+          final AuthenticationService authenticationService =
+              ref.watch(authenticationServiceFirebaseProvider);
 
-        if (isEmailVerified) {
-          final notifier =
-              ref.watch(firebaseAuthUserStateNotifierProvider.notifier);
+          final bool isEmailVerified =
+              await authenticationService.currentUserGetEmailVerified();
 
-          notifier.updateEmailVerified(isEmailVerified);
+          if (isEmailVerified) {
+            final firebaseAuthUserStateNotifier =
+                ref.watch(firebaseAuthUserStateNotifierProvider.notifier);
 
-          timer.cancel();
+            firebaseAuthUserStateNotifier.updateEmailVerified(isEmailVerified);
+
+            timer.cancel();
+          }
+        } on firebase_auth.FirebaseAuthException catch (e) {
+          switch (e.code) {
+            case "current-user-null":
+              timer.cancel();
+
+              break;
+
+            default:
+              break;
+          }
         }
       },
     );
