a97b658cefb06cc101497a421ffce700fddd1781
konbu33 <konbu33@gmail.com>
2022-11-30 11:38:15 +0900

    refactor: ロジックをリファクタリング

--- lib/rtc_video/presentation/widget/agora_video_local_preview_widget.dart
+++ lib/rtc_video/presentation/widget/agora_video_local_preview_widget.dart
@@ -1,41 +1,32 @@
-// import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:agora_rtc_engine/rtc_local_view.dart' as rtc_local_view;
+import 'package:agora_rtc_engine/rtc_local_view.dart';
 
 class AgoraVideoLocalPreviewWidget extends StatelessWidget {
   const AgoraVideoLocalPreviewWidget({
     Key? key,
-    required this.isJoinedChannel,
   }) : super(key: key);
-
-  final bool isJoinedChannel;
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return AgoraVideoView(
-  //     controller: VideoViewController(
-  //       rtcEngine: createAgoraRtcEngine(),
-  //       canvas: VideoCanvas(uid: state.localUid),
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
-    if (isJoinedChannel && defaultTargetPlatform == TargetPlatform.android ||
-        isJoinedChannel && defaultTargetPlatform == TargetPlatform.iOS) {
-      return const rtc_local_view.SurfaceView();
-    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return const SurfaceView();
 
-    if (isJoinedChannel && defaultTargetPlatform == TargetPlatform.windows ||
-        isJoinedChannel && defaultTargetPlatform == TargetPlatform.macOS) {
-      return const rtc_local_view.TextureView();
-    } else {
-      return const Text(
-        'Please join channel first',
-        textAlign: TextAlign.center,
-      );
+      case TargetPlatform.iOS:
+        return const SurfaceView();
+
+      case TargetPlatform.windows:
+        return const TextureView();
+
+      case TargetPlatform.macOS:
+        return const TextureView();
+
+      default:
+        return const Text(
+          'Please join channel first',
+          textAlign: TextAlign.center,
+        );
     }
   }
 }
--- lib/rtc_video/presentation/widget/agora_video_remote_preview_widget.dart
+++ lib/rtc_video/presentation/widget/agora_video_remote_preview_widget.dart
@@ -1,7 +1,7 @@
 // import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:agora_rtc_engine/rtc_remote_view.dart' as rtc_remote_view;
+import 'package:agora_rtc_engine/rtc_remote_view.dart';
 
 class AgoraVideoRemotePreviewWidget extends StatelessWidget {
   const AgoraVideoRemotePreviewWidget({
@@ -13,48 +13,30 @@ class AgoraVideoRemotePreviewWidget extends StatelessWidget {
   final String channelName;
   final int remoteUid;
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   // if (state.remoteUid != null) {
-  //   return AgoraVideoView(
-  //     controller: VideoViewController.remote(
-  //       rtcEngine: createAgoraRtcEngine(),
-  //       canvas: VideoCanvas(uid: state.remoteUid),
-  //       connection: RtcConnection(
-  //         channelId: state.channelName,
-  //         localUid: state.localUid,
-  //       ),
-  //     ),
-  //   );
-  //   //   } else {
-  //   //     return const Text(
-  //   //       'Please wait for remote user to join',
-  //   //       textAlign: TextAlign.center,
-  //   //     );
-  //   //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
-    if (remoteUid != 0 && defaultTargetPlatform == TargetPlatform.android ||
-        remoteUid != 0 && defaultTargetPlatform == TargetPlatform.iOS) {
-      return rtc_remote_view.SurfaceView(
-        uid: remoteUid,
-        channelId: channelName,
-      );
-    }
+    const notExistRemoteUserWidget = Text(
+      'Please wait remote user join',
+      textAlign: TextAlign.center,
+    );
 
-    if (remoteUid != 0 && defaultTargetPlatform == TargetPlatform.windows ||
-        remoteUid != 0 && defaultTargetPlatform == TargetPlatform.macOS) {
-      return rtc_remote_view.TextureView(
-        uid: remoteUid,
-        channelId: channelName,
-      );
-    } else {
-      return const Text(
-        'Please wait remote user join',
-        textAlign: TextAlign.center,
-      );
+    if (remoteUid == 0) return notExistRemoteUserWidget;
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return SurfaceView(uid: remoteUid, channelId: channelName);
+
+      case TargetPlatform.iOS:
+        return SurfaceView(uid: remoteUid, channelId: channelName);
+
+      case TargetPlatform.windows:
+        return TextureView(uid: remoteUid, channelId: channelName);
+
+      case TargetPlatform.macOS:
+        return TextureView(uid: remoteUid, channelId: channelName);
+
+      default:
+        return notExistRemoteUserWidget;
     }
   }
 }
