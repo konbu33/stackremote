2f0d3f8a491c76687f7276d8d419f442dd44ef6b
konbu33 <konbu33@gmail.com>
2023-01-06 11:30:33 +0900

    refactor: 不要なコード削除

--- test/goldens_test/name_field_widget_test.dart
+++ test/goldens_test/name_field_widget_test.dart
@@ -50,13 +50,6 @@ void main() {
         nameFieldStateNotifierProvider: loginIdFieldStateNotifierProvider,
       );
 
-      // final LoginIdFieldStateProvider loginIdFieldStateProvider =
-      //     loginIdFieldStateNotifierProviderCreator();
-
-      // final widget = LoginIdFieldWidget(
-      //   loginIdFieldStateProvider: loginIdFieldStateProvider,
-      // );
-
       await tester.pumpWidgetBuilder(
           ProviderScope(
             child: MaterialApp(
@@ -86,43 +79,3 @@ void main() {
     });
   });
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:golden_toolkit/golden_toolkit.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-// import 'package:stackremote/home_page.dart';
-
-// void main() {
-//   String title = "home_page";
-//   group("$title group", () {
-//     testGoldens("$title test", (WidgetTester tester) async {
-//       // given
-//       const size = Size(414, 896);
-
-//       // フォント読み込み
-//       await loadAppFonts();
-
-//       await tester.pumpWidgetBuilder(const ProviderScope(child: HomePage()),
-//           surfaceSize: size);
-
-//       String idString = "";
-//       String outFileName = "";
-
-//       // when initial
-//       idString = "initial";
-//       outFileName = "${title}_$idString";
-//       await screenMatchesGolden(tester, outFileName);
-
-//       // when increment
-//       await tester.tap(find.byIcon(Icons.add));
-//       await tester.pump();
-
-//       idString = "increment";
-//       outFileName = "${title}_$idString";
-//       await screenMatchesGolden(tester, outFileName);
-//     });
-//   });
-// }
-
